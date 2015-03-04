5c4796daae3fffc8d76424a74a8b325dcbb7a893
Alexander Kaloshin <alexander.kaloshin@gmail.com>
2015-03-04 17:43:29 +0300

    test

--- app.js
+++ app.js
@@ -14,27 +14,3 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
-
-console.log('try get redis');
-
-var _redis = require('redis');
-
-var host = process.env.REDIS_HOST || 'redis-1.demo.dev';
-var port = process.env.REDIS_PORT || '49261';
-var db = process.env.REDIS_DB     || 0;
-
-console.log('try to connect');
-
-var connection = _redis.createClient(port, host);
-
-connection.on('connect', function () {
-  console.log('redis connected', host, port, 'database:', db);
-});
-
-connection.on('error', function (err) {
-  console.log(err);
-});
-
-connection.on('close', function(){
-  console.log('close redis connection');
-});
