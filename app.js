d162681066f83a1bf617e08b175374b453f603a8
Alexander Kaloshin <alexander.kaloshin@gmail.com>
2015-04-23 20:26:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var server = app.listen(3000, function () {
   console.log('Example app listening');
 
 });
-
+/*
 var host = process.env.REDIS_HOST;
 var port = process.env.REDIS_PORT;
 console.log('connect to client: ', host, ":", port );
@@ -28,3 +28,4 @@ client.on('connect', function (){
     console.log('authenticated');
   })
 });
+*/
