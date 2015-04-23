68df24db6c1b526b2765a24a8103e11e7659c2af
Alexander Kaloshin <alexander.kaloshin@gmail.com>
2015-04-23 20:16:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  console.log('demo');
+  res.send('Hello World #1!');
 });
 
 console.log('Hello world: Demo test');
