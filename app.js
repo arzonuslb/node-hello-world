98a9f819c7326bd5403ce60799a54880d4a32a47
Alexander Kaloshin <alexander.kaloshin@gmail.com>
2015-08-26 00:19:40 +0300

    Disable redis

--- app.js
+++ app.js
@@ -20,12 +20,14 @@ var server = app.listen(3000, function () {
   console.log('Example app listening');
 
 });
-
+/*
 var host = process.env.REDIS_HOST;
 var port = process.env.REDIS_PORT;
 console.log('connect to client: ', host, ":", port );
 var redis = require("redis"), client = redis.createClient(port, host);
 client.auth(process.env.REDIS_AUTH);
 client.on('connect', function (){
+
   console.log('connected to Redis');
 });
+*/
