193ea65f98f7866cd7f4d64be4caee5b8c32e214
Alexander Kaloshin <alexander.kaloshin@gmail.com>
2015-12-30 14:20:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var i = 0;
+var i = 5;
 app.get('/', function (req, res) {
   console.log('request #',i);
   res.send({request: i, state:1});
